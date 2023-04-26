b29065bfd7278c9245c3b7eedcde050aa1939b0b
Marko Vejnovic <contact@markovejnovic.com>
2023-04-26 01:17:40 -0500

    Migrated tests to test against Cxx orpheus

--- test/test_sine16.cpp
+++ test/test_sine16.cpp
@@ -2,22 +2,14 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 #include "Vorpheus_sine16.h"
+#include "orpheus_util.hpp"
 
 TEST_CASE("Test sin16 against Orpheus") {
   auto model = std::make_unique<Vorpheus_sine16>();
   for (std::int16_t i = 0; i < std::numeric_limits<int16_t>::max(); i++) {
     model->angle_input = i;
     model->eval();
-
-    const auto actualSine = model->o;
-
-    const auto expectedSine = sin(
-      2 * std::numbers::pi *
-      static_cast<double>(i) / static_cast<double>(std::numeric_limits<int16_t>::max())
-    );
-
-    REQUIRE(static_cast<double>(static_cast<int16_t>(actualSine)) / 4096.0
-      == Approx(expectedSine).margin(2 / 4096.0));
+    REQUIRE(static_cast<int16_t>(model->o) == Math::Fixed::sin(i));
   }
 }
 
