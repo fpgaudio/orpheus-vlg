d5f3ea160b08a47457f1c8b270b16d66a6239ac2
Marko Vejnovic <contact@markovejnovic.com>
2023-05-05 01:04:43 -0500

    Print input-output states

--- test/test_mod_sine16.cpp
+++ test/test_mod_sine16.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
 #include <memory>
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
@@ -6,10 +9,22 @@
 
 TEST_CASE("Test sin16 against Orpheus") {
   auto model = std::make_unique<Vmod_sine16>();
+
+  std::ofstream inputStream;
+  inputStream.open("mod_sine16_i_angle.txt");
+  std::ofstream outputStream;
+  outputStream.open("mod_sine16_o_sine.txt");
+
   for (std::int16_t i = 0; i < std::numeric_limits<int16_t>::max(); i++) {
     model->i_angle = i;
     model->eval();
     REQUIRE(static_cast<int16_t>(model->o_sine) == Math::Fixed::sin(i));
+
+    inputStream << std::setw(4) << std::setfill('0') << std::hex << model->i_angle << std::endl;
+    outputStream << std::setw(4) << std::setfill('0') << std::hex << model->o_sine << std::endl;
   }
+
+  inputStream.close();
+  outputStream.close();
 }
 
