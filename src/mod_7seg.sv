a20b78c2784394cfc324fd7b5217b1465fd2f022
Marko Vejnovic <contact@markovejnovic.com>
2023-05-16 19:37:18 -0500

    mod_7seg: Explicit bit widths for Quartus

--- src/mod_7seg.sv
+++ src/mod_7seg.sv
@@ -1,5 +1,5 @@
 module mod_7seg#
-  ( parameter DEFAULT_STATE = 'b0000000 // The default state.
+  ( parameter DEFAULT_STATE = 7'b0000000 // The default state.
   ) ( output logic [0:6] o_segments // The individual segments,
                                   // clockwise, top first.
   , input logic [3:0] i_value // Input value
@@ -12,53 +12,53 @@ module mod_7seg#
       o_segments <= ~DEFAULT_STATE;
     end else begin
       case (i_value)
-        0: begin
-          o_segments <= ~'b1111110;
+        4'h0: begin
+          o_segments <= ~7'b1111110;
         end
-        1: begin
-          o_segments <= ~'b0110000;
+        4'h1: begin
+          o_segments <= ~7'b0110000;
         end
-        2: begin
-          o_segments <= ~'b1101101;
+        4'h2: begin
+          o_segments <= ~7'b1101101;
         end
-        3: begin
-          o_segments <= ~'b1111001;
+        4'h3: begin
+          o_segments <= ~7'b1111001;
         end
-        4: begin
-          o_segments <= ~'b0110011;
+        4'h4: begin
+          o_segments <= ~7'b0110011;
         end
-        5: begin
-          o_segments <= ~'b1011011;
+        4'h5: begin
+          o_segments <= ~7'b1011011;
         end
-        6: begin
-          o_segments <= ~'b1011111;
+        4'h6: begin
+          o_segments <= ~7'b1011111;
         end
-        7: begin
-          o_segments <= ~'b1110000;
+        4'h7: begin
+          o_segments <= ~7'b1110000;
         end
-        8: begin
-          o_segments <= ~'b1111111;
+        4'h8: begin
+          o_segments <= ~7'b1111111;
         end
-        9: begin
-          o_segments <= ~'b1111011;
+        4'h9: begin
+          o_segments <= ~7'b1111011;
         end
-        'ha: begin
-          o_segments <= ~'b1110111;
+        4'ha: begin
+          o_segments <= ~7'b1110111;
         end
-        'hb: begin
-          o_segments <= ~'b0011111;
+        4'hb: begin
+          o_segments <= ~7'b0011111;
         end
-        'hc: begin
-          o_segments <= ~'b1001110;
+        4'hc: begin
+          o_segments <= ~7'b1001110;
         end
-        'hd: begin
-          o_segments <= ~'b0111101;
+        4'hd: begin
+          o_segments <= ~7'b0111101;
         end
-        'he: begin
-          o_segments <= ~'b1001111;
+        4'he: begin
+          o_segments <= ~7'b1001111;
         end
-        'hf: begin
-          o_segments <= ~'b1000111;
+        4'hf: begin
+          o_segments <= ~7'b1000111;
         end
         default: begin
           o_segments <= ~DEFAULT_STATE;
